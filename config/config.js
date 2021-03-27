723651703f716f208fb5a84c6c88f6b2f904cb26
mogador26 <hassan.driss@magic.fr>
2021-03-27 10:21:01 +0100

    add parameter database

--- config/config.js
+++ config/config.js
@@ -5,5 +5,9 @@ module.exports = {
     port: process.env.PORT,
     maxRequestByIp: process.env.MAX_REQUEST_BY_IP,
     windowRequest: process.env.WINDOW_TIME,
-    dirLog: process.env.DIR_LOG
+    dirLog: process.env.DIR_LOG,
+    userDB: process.env.USER_DB,
+    passDB: process.env.PASSWORD_DB,
+    uriDB: process.env.URI_DB
+
 }
